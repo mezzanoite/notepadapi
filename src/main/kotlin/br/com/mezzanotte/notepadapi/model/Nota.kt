51575e9080216a54044fcf79f1fb155ebe0baf04
logonrm <logonrm@fiap.com.br>
2018-02-26 22:19:44 -0300

    Salvando ...

--- src/main/kotlin/br/com/mezzanotte/notepadapi/model/Nota.kt
+++ src/main/kotlin/br/com/mezzanotte/notepadapi/model/Nota.kt
@@ -4,6 +4,6 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document
-data class Nota(@Id var id: String,
+data class Nota(@Id var id: String? = null,
                 var titulo: String,
                 var conteudo: String)
